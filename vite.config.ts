247cbc7413fdc324d1c0961a3311eac6ca1010a7
Seah Ying Hang <seahyinghang8@gmail.com>
2023-04-17 01:59:25 -0700

    config workaround to allow webworker deployment

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,20 @@ export default defineConfig({
 	plugins: [sveltekit()],
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
+	},
+	worker: {
+		plugins: [
+			{
+				name: 'remove-manifest',
+				configResolved(c) {
+					const manifestPlugin = c.worker.plugins.findIndex((p) => p.name === 'vite:manifest');
+					c.worker.plugins.splice(manifestPlugin, 1);
+					const ssrManifestPlugin = c.worker.plugins.findIndex(
+						(p) => p.name === 'vite:ssr-manifest'
+					);
+					c.plugins.splice(ssrManifestPlugin, 1);
+				}
+			}
+		]
 	}
 });
